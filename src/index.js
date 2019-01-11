30c6c5013b4b096d00bc1f1037dd902f8e0b0c0c
Riccardo-Zanutta <zanuttar@gmail.com>
2019-01-11 16:07:34 +0100

    perf(core functions): Add $, $1, each, getClosest, insertAfter functionalities

--- src/index.js
+++ src/index.js
@@ -7,3 +7,8 @@
 
 export { default as debounce } from './scripts/debounce'
 export { default as throttle } from './scripts/throttle'
+export { default as each } from './scripts/each'
+export { default as $ } from './scripts/$'
+export { default as $1 } from './scripts/$1'
+export { default as getClosest } from './scripts/getClosest'
+export { default as insertAfter } from './scripts/insertAfter'
